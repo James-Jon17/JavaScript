cc0bcf5c6764b95be24bbe1cd39e69d5bb0b79f8
James-Jon17 <117294045+James-Jon17@users.noreply.github.com>
2022-11-16 13:08:03 -0300

    Att ex 12 JS

---
+++ aula12/ex013.js
@@ -0,0 +1,43 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+}
+switch(diaSem) {
+    case 1:
+        console.log('Segunda')
+        break
+}
+switch(diaSem) {
+    case 2:
+        console.log('Terça')
+        break
+}
+switch(diaSem) {
+    case 3:
+        console.log('Quarta')
+        break
+}
+switch(diaSem) {
+    case 4:
+        console.log('Quinta')
+        break
+}
+switch(diaSem) {
+    case 5:
+        console.log('Sexta')
+        break
+}
+switch(diaSem) {
+    case 6:
+        console.log('Sabado')
+        break
+}
+switch(diaSem) {
+    case 7:
+        console.log('[ERRO] Dia Invalido')
+        break
+}
