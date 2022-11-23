06c2ea9a7ac8a557111156f7236cccbc3c47b332
João Alcântara <117294045+James-Jon17@users.noreply.github.com>
2022-11-23 07:50:25 -0300

    tabuada simples

---
+++ ex16 copy/script/script.js
@@ -0,0 +1,13 @@
+function tabuada() {
+    let num = document.querySelector('#txtn')
+    let tab = document.querySelector('#seltab')
+    if (num.value.length == 0 ) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        for(let c = 1; c<=10; c++) {
+
+        }
+        tab.innerHTML = '' 
+        }
+    }
